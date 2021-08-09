fac16e6bbd8cafa4eae0c2ac650a7f4bb6115e0c
staniboy <stanley.v@outlook.com>
2021-08-09 15:52:31 +0400

    feat: sortedSubmitions computed prop

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const upvoteApp = {
       submissions: Seed.submissions,
     };
   },
+  computed: {
+    sortedSubmitions() {
+      return this.submissions.sort((a, b) => {
+        return b.votes - a.votes;
+      });
+    },
+  },
 };
 
 Vue.createApp(upvoteApp).mount("#app");
