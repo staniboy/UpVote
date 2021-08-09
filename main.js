2c953e2a7e0b338567973ed633bf7cdf9a78806d
staniboy <stanley.v@outlook.com>
2021-08-09 16:35:50 +0400

    feat: submissionComponent

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
+const submissionComponent = {
+  template: `<div Style="display: flex; width: 100%;">
+    <figure class="media-left">
+            <img class="image is-64x64" :src="submission.submissionImage" />
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>
+                  <a href="#" class="has-text-info">{{ submission.title }}</a>
+                  <span class="tag is-small">#{{ submission.id }}</span>
+                </strong>
+                <br />
+                {{ submission.description }}
+                <br />
+                <small class="is-size-7">
+                  Submitted by:
+                  <img class="image is-24x24" :src="submission.avatar" />
+                </small>
+              </p>
+            </div>
+          </div>
+          <div class="media-right">
+            <span class="icon is-small" @click="upvote(submission.id)">
+              <i class="fa fa-chevron-up"></i>
+              <strong class="has-text-info">{{ submission.votes }}</strong>
+            </span>
+          </div>
+    </div>`,
+};
+
 const upvoteApp = {
+  components: {
+    "submission-component": submissionComponent,
+  },
   data() {
     return {
       submissions: Seed.submissions,
